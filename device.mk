ef558a41999a100eff90bfd199f88093af8b0ed6
bychay <94874469+bychay@users.noreply.github.com>
2025-08-17 18:19:13 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -105,5 +105,5 @@ PRODUCT_PACKAGES += \
     vendor.meizu.hardware.inscreen.hal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/meizu-hals/doze/vendor.meizu.hardware.doze.hal.rc:vendor/etc/init/vendor.meizu.hardware.doze.hal.rc \
-    $(LOCAL_PATH)/meizu-hals/doze/vendor.meizu.hardware.inscreen.hal.rc:vendor/etc/init/vendor.meizu.hardware.inscreen.hal.rc
+    $(LOCAL_PATH)/meizu-hals/hbm/vendor.meizu.hardware.inscreen.hal.rc:vendor/etc/init/vendor.meizu.hardware.inscreen.hal.rc
 
